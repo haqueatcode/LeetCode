83365cdedfa1613eea6078f0eef3ee3a86b14f69
EatCode <enamul_cse@yahoo.com>
2022-06-26 15:24:22 -0700

    Leetcode 232

---
+++ 232. Implement Queue using Stacks/232.py
@@ -0,0 +1,9 @@
+"""
+Implement a first in first out (FIFO) queue using only two stacks. 
+The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+"""
+
+if __name__ == "__main__":
+    
+    print("IMplementing a Queue using two Stacks")
+
