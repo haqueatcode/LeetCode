ff915c78b19318220929fe16796fad6934578d11
EatCode <enamul_cse@yahoo.com>
2022-06-29 17:57:09 -0700

    Update 20.py

--- 0020. Valid Parentheses/20.py
+++ 0020. Valid Parentheses/20.py
@@ -31,6 +31,50 @@ Constraints:
 
 1 <= s.length <= 10^4
 s consists of parentheses only '()[]{}'.
+
+
+Solution idea:
+    push one charaster at a time to a stack
+    check if its pair is on top of the stack
+    if found pop the top
+    
+    stop when input is empty and check if stack is empty--> valid
+    else invalid
 """
 
+class ValidParentheses:
+    
+    def __init__(self, stream):
+        self.stream = stream
+        self.stack = []
 
+    
+    def eval_stream(self):
+        # separate string into individual character
+        items = list(self.stream)
+        if len(self.stack) > 0 :
+            stack_top = self.stack[-1]
+            
+        if self.stream:
+            # take each item
+            for item in items:
+                
+                
+                if stack_top == "(" and item == ")":
+                    self.stack.pop()
+                elif stack_top == "{" and item == "}":
+                    self.stack.pop()
+                elif stack_top == "[" and item == "]":
+                    self.stack.pop()
+                else:
+                    self.stack.append(item)
+            
+
+if __name__ == "__main__":
+    
+    stream = "()"
+    
+
+    
+    vp = ValidParentheses(stream)
+    vp.eval_stream()
